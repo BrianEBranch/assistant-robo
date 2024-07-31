d3ca725944777914a69364c055d943b99174ba78
lemokelw <brianramos892@gmail.com>
2024-07-31 14:42:05 -0400

    added default responses so that it doesn't keep saving responses.

--- main.py
+++ main.py
@@ -24,28 +24,34 @@ mic = sr.Microphone()
 
 # get os name, in order to determine which functions are to be used since some are platform dependent.
 opSystem = platform.system()
-
 # create gpt assistant object
 client = OpenAI()
+# create thread, basically a conversation between user and ai.
+# if thread is created up here, stanley will retain memory of previous conversations.
+thread = client.beta.threads.create()
 stanley = client.beta.assistants.create(
     instructions="You are Stanley, a helpful assistant tasked with helping the user in any safe way possible. RESPOND "
                  "AS CONCISE AS POSSIBLE.",
     name="Stanley",
     description="Stanley is a jarvis like AI-Assistant meant to help the user with day to day task.",
     model="gpt-4o-mini",
-    temperature=0.3,
 )
 
 
 # speak is a function that takes in some text and writes that text to our mp3 file which gets converted to tts
 def speak(text):
     tts = gTTS(text=text, lang='en')
-    formattedTime = datetime.now().strftime("%I:%M:%S %p")
-    fileName = f"responses_on_{formattedTime}.mp3"
-    #remove characters that are invalid for filenames
-    fileName = re.sub(r'[<>:"/\\|?*]', '', fileName)
-    tts.save(f"responses/{fileName}")
-    playsound(f"responses/{fileName}")
+    if(text == "Hello, I am at your service"):
+        playsound("responses/stanley_hello.mp3")
+    elif(text == "I couldn't understand"):
+        playsound("responses/stanley_error.mp3")
+    else:
+        formattedTime = datetime.now().strftime("%I:%M:%S %p")
+        fileName = f"responses_on_{formattedTime}.mp3"
+        #remove characters that are invalid for filenames
+        fileName = re.sub(r'[<>:"/\\|?*]', '', fileName)
+        tts.save(f"responses/{fileName}")
+        playsound(f"responses/{fileName}")
 
 
 # get_gpt3_response takes in a some prompt and generates a response using the appropriate parameters.
@@ -91,9 +97,6 @@ audio_stream = pa.open(
 )
 
 try:
-    # create thread, basically a conversation between user and ai.
-    # if thread is created up here, stanley will retain memory of previous conversations.
-    thread = client.beta.threads.create()
     while True:
         pcm = audio_stream.read(porcupine.frame_length)
         pcm = struct.unpack('h' * porcupine.frame_length, pcm)
@@ -101,10 +104,9 @@ try:
         if keyword_index >= 0:
             speak("Hello, I am at your service")
             with sr.Microphone() as source:
-                audio = recognizer.listen(source, 10, 5)
+                audio = recognizer.listen(source, None, 6)
                 try:
                     text = recognizer.recognize_google(audio)
-                    # on each iteration create a new message from user request
                     get_gpt3_response(text)
                 except sr.UnknownValueError:
                     speak("I couldn't understand")
