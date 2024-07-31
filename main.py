d60034d6e3c9d2f4e3364f98979f3bf1e0e91610
lemokelw <brianramos892@gmail.com>
2024-07-31 14:18:17 -0400

    STANLEY FINALLY SPEAKS!!!

--- main.py
+++ main.py
@@ -5,49 +5,72 @@ import pvporcupine
 import pyaudio
 import struct
 import platform
-import time
 from gtts import gTTS
 from playsound import playsound
+from dotenv import load_dotenv
+from datetime import datetime
 import os
+import run
+import re
 
 # get necessary keys from system env variables.
-openai.api_key =
-porcupine_access_key =
+load_dotenv()
+openai.api_key = "OPENAI_API_KEY"
+porcupine_access_key = "PORCUPINE_ACCESS_KEY"
 
 # create recognizer object and microphone object from speech_recognition library.
 recognizer = sr.Recognizer()
 mic = sr.Microphone()
 
-#get os name, in order to determine which functions are to be used since some are platform dependent.
+# get os name, in order to determine which functions are to be used since some are platform dependent.
 opSystem = platform.system()
 
-#create gpt assistant object
+# create gpt assistant object
 client = OpenAI()
 stanley = client.beta.assistants.create(
     instructions="You are Stanley, a helpful assistant tasked with helping the user in any safe way possible. RESPOND "
                  "AS CONCISE AS POSSIBLE.",
     name="Stanley",
     description="Stanley is a jarvis like AI-Assistant meant to help the user with day to day task.",
-    tools=[{"type": "code_interpreter"}],
     model="gpt-4o-mini",
     temperature=0.3,
 )
-# get_gpt3_response takes in a some prompt and generates a response using the appropriate parameters.
-# then returns the first element in choices since gpt responses are arrays and strips the whitespace from the response.
-def get_gpt3_response(prompt):
-    print("fix")
-    #currently needs ot be updated since openai no longer supports completions
 
 
 # speak is a function that takes in some text and writes that text to our mp3 file which gets converted to tts
 def speak(text):
     tts = gTTS(text=text, lang='en')
-    tts.save("responses/response.mp3")
-    playsound("responses/response.mp3")
+    formattedTime = datetime.now().strftime("%I:%M:%S %p")
+    fileName = f"responses_on_{formattedTime}.mp3"
+    #remove characters that are invalid for filenames
+    fileName = re.sub(r'[<>:"/\\|?*]', '', fileName)
+    tts.save(f"responses/{fileName}")
+    playsound(f"responses/{fileName}")
 
-if(opSystem=="Windows"):
+
+# get_gpt3_response takes in a some prompt and generates a response using the appropriate parameters.
+# then returns the first element in choices since gpt responses are arrays and strips the whitespace from the response.
+def get_gpt3_response(text):
+    # on each iteration create a new message from user request
+    event_handler = run.EventHandler()
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=text
+    )
+    with client.beta.threads.runs.stream(
+            thread_id=thread.id,
+            assistant_id=stanley.id,
+            instructions="You are Stanley, a helpful assistant tasked with helping the user in any safe way possible."
+                         "RESPOND AS CONCISE AS POSSIBLE.",
+            event_handler=event_handler,
+    ) as stream:
+        stream.until_done()
+        speak(event_handler.generated_text)
+
+if (opSystem == "Windows"):
     kPath = ['windows/Hey-Stanley_en_windows_v3_0_0(1)/Hey-Stanley_en_windows_v3_0_0.ppn']
-elif(opSystem=="Darwin"):
+elif (opSystem == "Darwin"):
     kPath = ['mac/Hey-Stanley_en_mac_v3_0_0/Hey-Stanley_en_mac_v3_0_0.ppn']
 else:
     kPath = ['placeholder for raspOs ppn/Unix']
@@ -68,21 +91,21 @@ audio_stream = pa.open(
 )
 
 try:
+    # create thread, basically a conversation between user and ai.
+    # if thread is created up here, stanley will retain memory of previous conversations.
+    thread = client.beta.threads.create()
     while True:
         pcm = audio_stream.read(porcupine.frame_length)
         pcm = struct.unpack('h' * porcupine.frame_length, pcm)
         keyword_index = porcupine.process(pcm)
         if keyword_index >= 0:
+            speak("Hello, I am at your service")
             with sr.Microphone() as source:
-                speak("Hello, I am at your service")
-                audio = recognizer.listen(source,10,5)
-                print(audio)
-                print("here")
+                audio = recognizer.listen(source, 10, 5)
                 try:
-                    print("try here")
                     text = recognizer.recognize_google(audio)
-                    response = get_gpt3_response(text)
-                    speak(response)
+                    # on each iteration create a new message from user request
+                    get_gpt3_response(text)
                 except sr.UnknownValueError:
                     speak("I couldn't understand")
                 except sr.RequestError as e:
---
+++ run.py
@@ -0,0 +1,33 @@
+# this file is meant to be used for creating and streaming a run from an assistant
+from openai.types.beta.threads import Text
+from typing_extensions import override
+from openai import AssistantEventHandler
+
+
+class EventHandler(AssistantEventHandler):
+
+    def __init__(self):
+        self.generated_text = ""
+        super().__init__()
+
+    @override
+    def on_text_created(self, text) -> None:
+        print(f"\nassistant > ", end="", flush=True)
+
+    @override
+    def on_text_delta(self, delta, snapshot):
+        self.generated_text += delta.value
+        print(delta.value, end="", flush=True)
+
+    def on_tool_call_created(self, tool_call):
+        print(f"\nassistant > {tool_call.type}\n", flush=True)
+
+    def on_tool_call_delta(self, delta, snapshot):
+        if delta.type == 'code_interpreter':
+            if delta.code_interpreter.input:
+                print(delta.code_interpreter.input, end="", flush=True)
+            if delta.code_interpreter.outputs:
+                print(f"\n\noutput >", flush=True)
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "logs":
+                        print(f"\n{output.logs}", flush=True)
