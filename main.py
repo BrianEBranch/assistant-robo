cac6d64cbbbd1e8b9f3fdb86b683696cae2103c5
lemokelw <brianramos892@gmail.com>
2024-07-31 10:00:39 -0400

    curr working on creating an OpenAi assistant object. reworking paths so that it changes model based on OS.

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
 import openai
+from openai import OpenAI
 import speech_recognition as sr
 import pvporcupine
 import pyaudio
 import struct
+import platform
 import time
 from gtts import gTTS
+from playsound import playsound
 import os
 
 # get necessary keys from system env variables.
-openai.api_key = os.getenv("OPENAI_API_KEY")
-porcupine_access_key = os.getenv("PORCUPINE_ACCESS_KEY")
+openai.api_key =
+porcupine_access_key =
 
 # create recognizer object and microphone object from speech_recognition library.
 recognizer = sr.Recognizer()
 mic = sr.Microphone()
 
+#get os name, in order to determine which functions are to be used since some are platform dependent.
+opSystem = platform.system()
 
+#create gpt assistant object
+client = OpenAI()
+stanley = client.beta.assistants.create(
+    instructions="You are Stanley, a helpful assistant tasked with helping the user in any safe way possible. RESPOND "
+                 "AS CONCISE AS POSSIBLE.",
+    name="Stanley",
+    description="Stanley is a jarvis like AI-Assistant meant to help the user with day to day task.",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4o-mini",
+    temperature=0.3,
+)
 # get_gpt3_response takes in a some prompt and generates a response using the appropriate parameters.
 # then returns the first element in choices since gpt responses are arrays and strips the whitespace from the response.
 def get_gpt3_response(prompt):
-    response = openai.Completion.create(
-        engine="gpt-4o-mini",
-        prompt=prompt,
-        max_tokens=150,
-    )
-    return response.choices[0].text.strip()
+    print("fix")
+    #currently needs ot be updated since openai no longer supports completions
 
 
 # speak is a function that takes in some text and writes that text to our mp3 file which gets converted to tts
 def speak(text):
     tts = gTTS(text=text, lang='en')
     tts.save("responses/response.mp3")
-    os.system("afplay responses/response.mp3")
+    playsound("responses/response.mp3")
 
+if(opSystem=="Windows"):
+    kPath = ['windows/Hey-Stanley_en_windows_v3_0_0(1)/Hey-Stanley_en_windows_v3_0_0.ppn']
+elif(opSystem=="Darwin"):
+    kPath = ['mac/Hey-Stanley_en_mac_v3_0_0/Hey-Stanley_en_mac_v3_0_0.ppn']
+else:
+    kPath = ['placeholder for raspOs ppn/Unix']
 
 porcupine = pvporcupine.create(
     access_key=porcupine_access_key,
-    keyword_paths=['Hey-Stanley_en_mac_v3_0_0/Hey-Stanley_en_mac_v3_0_0.ppn']
+    keyword_paths=kPath
 )
 
 pa = pyaudio.PyAudio()
@@ -49,7 +67,6 @@ audio_stream = pa.open(
     frames_per_buffer=porcupine.frame_length
 )
 
-
 try:
     while True:
         pcm = audio_stream.read(porcupine.frame_length)
@@ -58,8 +75,11 @@ try:
         if keyword_index >= 0:
             with sr.Microphone() as source:
                 speak("Hello, I am at your service")
-                audio = recognizer.listen(source)
+                audio = recognizer.listen(source,10,5)
+                print(audio)
+                print("here")
                 try:
+                    print("try here")
                     text = recognizer.recognize_google(audio)
                     response = get_gpt3_response(text)
                     speak(response)
